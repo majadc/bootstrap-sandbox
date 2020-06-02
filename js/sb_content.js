a217a065e8ec7da12534d1e326086e143725c476
majadc <maja.chabros@gmail.com>
2020-06-02 11:07:29 +0200

    content code

--- js/sb_content.js
+++ js/sb_content.js
@@ -1,6 +1,22 @@
 $(document).ready(function(){
 
-  $('#text-truncate').find('.output-js').html(cssRulesMajadc.getCssStyleRules('bootstrap.min.css', '.text-truncate'));
+ 
+  let bootstrapCssFile = 'bootstrap.min.css';
+  let codeOutput = cssRulesMajadc.getCssStyleRules(bootstrapCssFile, 'code');
+  
+  let indexEnd = codeOutput.indexOf('}');
+  let codeCssRules = codeOutput.substring(0, indexEnd+1);
+  $('#code-output-js').html(codeCssRules);
+
+  $('#inline-code').find('.output-js').html(codeOutput.substring(indexEnd+1));
+
+  $('#block-code').find('.output-js').html(cssRulesMajadc.getCssStyleRules(bootstrapCssFile, 'pre').substring(indexEnd+1)).append(cssRulesMajadc.getCssStyleRules(bootstrapCssFile, 'pre code'));
+
+
+  $('#variables').find('.output-js').html(cssRulesMajadc.getCssStyleRules(bootstrapCssFile, 'var'));
+
+  $('#user-input').find('.output-js').html(cssRulesMajadc.getCssStyleRules(bootstrapCssFile, 'kbd').substring(indexEnd+1));
+  
 
 
 });//document ready
