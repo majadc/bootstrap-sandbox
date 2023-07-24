c5a307e40758b7c78e38a2b2470f0c3a4fb45285
majadc <maja.chabros@gmail.com>
2023-07-24 13:58:56 +0200

    no babel, adding eslint for checking javascript

--- footer.php
+++ footer.php
@@ -1,15 +1,15 @@
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 
-<script src="js/get_stylerules.js"></script>
-<script src="js/display-style.js"></script>
+<script src="js/general/get_stylerules.js"></script>
+<script src="js/general/display-style.js"></script>
 
-<script src="js/bs-breakpoints-viewport-state.js"></script>
-<script src="js/bs-breakpoints-list.js"></script>
-<script src="js/bs-breakpoints-media-queries-info.js"></script>
-<script src="js/bs-grid-media-query-classes.js"></script>
-<script src="js/bs-grid.js"></script>
-<script src="js/bs-main.js"></script>
+<script src="js/bs-layout/bs-breakpoints-viewport-state.js"></script>
+<script src="js/bs-layout/bs-breakpoints-list.js"></script>
+<script src="js/bs-layout/bs-breakpoints-media-queries-info.js"></script>
+<script src="js/bs-layout/bs-grid-media-query-classes.js"></script>
+<script src="js/bs-layout/bs-grid.js"></script>
+<script src="js/bs-layout/bs-layout-main.js"></script>
 </body>
 
 </html>
